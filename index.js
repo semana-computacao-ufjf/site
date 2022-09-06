f4723ff029716df01690b0c77983dcb1799a2ab4
Matheus-Avila <matheusavila1997@gmail.com>
2022-09-06 10:37:50 -0300

    programacao com menu iterativo funcionando

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ function changeColor(day)
     document.getElementById(day).style.backgroundColor = '#b01917';
     
 }
-changeColor("day1");
+
+function changeTable(tday){
+    collection = document.getElementsByClassName("table-day");
+    for (i in collection) {
+        console.log(collection[i])
+        if(i < 5){
+            collection[i].style.visibility='hidden';
+            collection[i].style.display='none';
+        }
+    };
+    document.getElementById(tday).style.display='block';
+    document.getElementById(tday).style.visibility='visible';
+}
+// changeColor("day1");
+document.getElementById("day1").click()
