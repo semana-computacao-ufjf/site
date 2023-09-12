9cfae8a4b41c18ed5ab8de4e9ee8140f939e5f0e
Isadora Gonçalves <isadora.goncalves@estudante.ufjf.br>
2023-09-12 13:06:31 -0300

    Feat: Adiciona app com footer e navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
