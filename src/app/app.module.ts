74c7efb745914cff31be34d0b571cbecd2a71593
Isadora Gonçalves <isadora.goncalves@estudante.ufjf.br>
2023-09-13 12:48:48 -0300

    Feat: Adiciona componentes novos aos módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,17 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ClockCtaComponent } from './clock-cta/clock-cta.component';
+import { EventDescriptionComponent } from './event-description/event-description.component';
 
 @NgModule({
-  declarations: [AppComponent, FooterComponent, NavbarComponent],
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    ClockCtaComponent,
+    EventDescriptionComponent,
+  ],
   imports: [BrowserModule, FontAwesomeModule],
   providers: [],
   bootstrap: [AppComponent],
