e060e935976ecddc2f2f039d94bae7eb7b5d96c5
Isadora Gonçalves <isadora.goncalves@estudante.ufjf.br>
2023-09-13 12:47:10 -0300

    Feat: Adiciona componente de descrição do evento

---
+++ src/app/event-description/event-description.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-description',
+  templateUrl: './event-description.component.html',
+  styleUrls: ['./event-description.component.css']
+})
+export class EventDescriptionComponent {
+
+}
