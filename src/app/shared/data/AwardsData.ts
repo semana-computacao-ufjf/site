8c1172908b3392847a1b8e2dbd399cc80ff43d81
Isadora Gonçalves <isadora.goncalves@estudante.ufjf.br>
2023-10-17 16:16:06 -0300

    Feat: Adiciona premiação

--- src/app/shared/data/AwardsData.ts
+++ src/app/shared/data/AwardsData.ts
@@ -3,4 +3,9 @@ import Award from '../classes/award';
 export const AwardsData: Award[] = [
   new Award('Rerum', ['R$ 3000', 'R$ 900', 'R$ 600']),
   new Award('Hackathon 4Matt', ['Alexa Echo Pop', 'Fire TV Stick', 'Fone JBL']),
+  new Award('Maratona de Programação', [
+    '3 Kindles 11ª Geração',
+    '3 Galaxy Buds 2',
+    'Brindes BITKA',
+  ]),
 ];
